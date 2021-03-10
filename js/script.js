0d006a249609eb00220ce676586c81e8176e6c7b
Carlito53 <69847147+Carlito53@users.noreply.github.com>
2021-03-10 08:49:26 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function dropdown_hover() {
+    var x = document.getElementById("demo");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
+  function dropdown_click() {
+    var x = document.getElementById("click");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
